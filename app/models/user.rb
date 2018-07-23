255bd663ac4051e0ac8feef3bb29710405b4ad03
eabrigham <eabrigham@gmail.com>
2018-07-23 15:08:58 -0400

    Used after_create method in user model to add a quiz for each card.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,20 @@ class User < ApplicationRecord
   has_many :examples
   has_many :quizzes
   has_many :cards, through: :quizzes
+
+  after_create :setup_quizzes, on: :create
+  
+  private
+    def setup_quizzes
+      Card.all.each do |card|
+        puts 'in setup_quizzes and running card '
+        puts card
+        self.quizzes.build(
+          :card_id => card.id,
+          :consecutive_correct => 0
+        )
+        self.save
+      end
+    end
+
 end
