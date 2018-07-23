b9f4637a74437feb825f371cc216e59fb2421492
eabrigham <eabrigham@gmail.com>
2018-07-23 18:35:32 -0400

    Altered quiz serializer
    Displayed consecutive_correct
    Removed state, prior_interval, next_test_day (will be used in a
    future version)

--- app/serializers/quiz_serializer.rb
+++ app/serializers/quiz_serializer.rb
@@ -1,5 +1,7 @@
 class QuizSerializer < ActiveModel::Serializer
-  attributes :id, :state, :prior_interval, :next_test_day
+  attributes :id, :consecutive_correct
+  # removed prior_interval, next_test_day, state
+  # will not be used until next version
   has_one :user
   has_one :card
 end
