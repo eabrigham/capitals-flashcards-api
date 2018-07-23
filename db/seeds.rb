d7925053afe7b927a2684cb7f6d00a2766e5e130
eabrigham <eabrigham@gmail.com>
2018-07-23 11:12:51 -0400

    Added csv file of european capitals and parsed into seed data
    Created a new Card for each line of the csv.

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'european-capitals.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+    c = Card.new
+    c.genre = row['Region']
+    c.side_a = row['Country']
+    c.side_b = row['Capital']
+    c.save
+    puts "card #{c.side_b}, #{c.side_a} in #{c.genre} saved"
+end
+
+puts "There are now #{Card.count} rows in the Card table"
